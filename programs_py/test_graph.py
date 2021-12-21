41ce6b856c9e9cc4b2af8934f04e03b3efb0e9bd
ovod18 <ovod18@yandex.ru>
2021-12-21 09:03:59 +0400

    файл test_graph.py строит линейный график продаж с разбивкой по годам

--- programs_py/test_graph.py
+++ programs_py/test_graph.py
@@ -7,10 +7,18 @@ def main():
 
     plt.plot(x_coords, y_coords)
     
-    plt.title('Образец данных')
-    plt.xlabel('Ось X')
-    plt.ylabel('Ось Y')
+    plt.title('Продажи с разбивкой по годам')
+    plt.xlabel('Год')
+    plt.ylabel('Объём продаж')
     plt.grid(True)
+# Настроить метки делений
+    plt.xticks([0, 1, 2, 3, 4], 
+               ['2016', '2017', '2018', '2019', '2020'])
+    plt.yticks([0, 1, 2, 3, 4, 5], 
+               ['$0m', '$1m', '$2m', '$3m', '$4m', '$5m'])
+# Задать границы осей    
+    plt.xlim(xmin=-1, xmax=10)
+    plt.ylim(ymin=-1, ymax=6)
 
     plt.show()
 

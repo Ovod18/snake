8bc4a32dc5b30c06bc0a16a560203cf699353e0f
ovod18 <ovod18@yandex.ru>
2021-12-29 15:58:24 +0400

    модифицирован test_game.py

--- programs_py/games_py/test_game.py
+++ programs_py/games_py/test_game.py
@@ -2,21 +2,26 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+# Задаем ширину и высоту
+WIDTH = 360    
+HEIGHT = 480   
 FPS = 30
+
 # Задаем цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
 # Создаем игру и окно
 pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+pygame.mixer.init() # Для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Задаем размер 
+#главного окна
+pygame.display.set_caption("My Game") # Задаем название главного окна
+clock = pygame.time.Clock() # Создаем объект типа часы
+
 # Цикл игры
 running = True
 while running:
@@ -28,7 +33,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False.quit()
 
+
     # рендеринг
     screen.fill(RED)
     # после отрисовки всего показываем каритнку на экране (переворачиваем экран)
+    
+    # рисуем прямоугольник белого цвета на главном окне
+    pygame.draw.rect(screen, WHITE, (20, 20, 100, 75))
+    # рисуем линию белого цвета на главном окне
+    pygame.draw.line(screen, WHITE, [10, 30], [290, 15], 3)
+    # обновление кадра# обновление кадра# обновление кадра                 
     pygame.display.flip()

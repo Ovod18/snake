a54f2b39680e0fa324b379587b15e11ed4e92c3d
Ovod18 <ovod18@yandex.ru>
2022-01-11 11:32:18 +0400

    When reaching the border of the screen, the snake dies in snake.py

--- programs_py/games_py/snake.py
+++ programs_py/games_py/snake.py
@@ -1,25 +1,25 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 220                                              # setting the FPS
-WHITE = (255, 255, 255)                               # setting the colors 
+DISPLAY_WIDTH = 360
+DISPLAY_HEIGHT = 480
+FPS = 20                                           # setting the FPS
+WHITE = (255, 255, 255)                             # setting the colors 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 SNAKE_WIDTH = 10
 
-pygame.init()                                         # creating the main window
+pygame.init()                                       # creating the main window
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((DISPLAY_WIDTH, 
+                                  DISPLAY_HEIGHT))
 pygame.display.set_caption("My snake")
 clock = pygame.time.Clock()
 
-
-x1 = 300                                            # Setting the coordinates 
-y1 = 300                                            # of snake.  
+x1 = DISPLAY_WIDTH  / 2                             # Setting the coordinates 
+y1 = DISPLAY_HEIGHT / 2                             # of snake.  
 x1_change = 0                                       # Setting the values of 
 y1_change = 0                                       # the coordinates change.
 
@@ -31,7 +31,6 @@ pygame.draw.rect(screen, GREEN, [x1, y1,
                                                     # Draw the snake
                                                     # in x1, y1 coordinates.
 pygame.display.update()                             # Update the display.
-
 
 running = True                                      # Creating the game cycle.
 while running:
@@ -53,6 +52,11 @@ while running:
             y1_change = 10
             x1_change = 0 
 
+    if ((x1 >= DISPLAY_WIDTH) or (x1 < 0) or
+           (y1 >= DISPLAY_HEIGHT) or
+           (y1 < 0)):
+        running = False 
+
     x1 += x1_change
     y1 += y1_change
 
@@ -63,7 +67,6 @@ while running:
 
     pygame.display.update()                         # Update the display.
     clock.tick(FPS) 
-
 
 pygame.quit()
 quit()

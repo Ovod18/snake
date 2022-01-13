a4d6917e41a43a4714bd365964981ddf0171b284
Ovod18 <ovod18@yandex.ru>
2022-01-13 21:34:03 +0400

    Returned the restrictions of the snakes movement in snake1.py

--- programs_py/games_py/snake1.py
+++ programs_py/games_py/snake1.py
@@ -23,8 +23,10 @@ pygame.display.set_caption("My snake")
 
 clock = pygame.time.Clock()
 
+"""Setting snakes properties"""
 my_snake = items.Snake(SNAKE_WIDTH)
-
+body_x = my_snake.get_body_x()
+body_y = my_snake.get_body_y()
 
 """Setting other values"""
 score = 0
@@ -52,15 +54,48 @@ while running:
                 running = False
     my_snake.set_course(course)
     my_snake.move()
-   # my_snake.eat()
+    if ((body_x[0]==apple.get_x()) and
+       (body_y[0]==apple.get_y())):
+        my_snake.eat()
+        apple.set_pos()
+        fps = fps + 1
+        score = score +1
 
+    """Wasted by collision a border."""
+    if ((body_x[0] >= DISPLAY_WIDTH) or (body_x[0] < 0) or
+           (body_y[0] >= DISPLAY_HEIGHT) or
+           (body_y[0] < 0)):
+         font = pygame.font.Font(None, 65)
+         text = "WASTED"
+         message = font.render(text, True, RED)
+         screen.blit(message, [X_CENTRE, Y_CENTRE])
+         pygame.display.update()
+         time.sleep(5)
+         running = False
 
+    """Wasted by collision the snakes body."""
+    for i in range(len(body_x)):
+        if i != 0:
+            if ((body_x[0] == body_x[i]) and
+               (body_y[0] == body_y[i])):
+                font = pygame.font.Font(None, 65)
+                text = "WASTED"
+                message = font.render(text, True, RED)
+                screen.blit(message, [X_CENTRE, Y_CENTRE])
+                pygame.display.update()
+                time.sleep(5)
+                running = False
+
+    """Frame out to the screen."""
     screen.fill(BLACK)
 
-    list_body_x = my_snake.get_body_x()
-    list_body_y = my_snake.get_body_y()
-    for i in range(len(list_body_x)):
-        pygame.draw.rect(screen, GREEN, [list_body_x[i], list_body_y[i],
+    font = pygame.font.Font(None, 30)
+    text = "Your score: " + str(score)
+    message = font.render(text, True, YELLOW)
+    screen.blit(message, [10, 10])
+
+    for i in range(len(body_x)):
+        pygame.draw.rect(screen, GREEN, [body_x[i], body_y[i],
                          SNAKE_WIDTH, SNAKE_WIDTH])
     pygame.draw.rect(screen, RED, [apple.get_x(), apple.get_y(),
                      SNAKE_WIDTH, SNAKE_WIDTH])

2531176c77751eb6e0ccff204cfae1e8808b4ed0
Ovod18 <ovod18@yandex.ru>
2022-01-23 14:07:12 +0400

    Removed snake.py, snake1.py. Snake2.py renamed to snake.py

--- programs_py/games_py/items.py
+++ programs_py/games_py/items.py
@@ -7,75 +7,6 @@ def dist(a, b):
 
 class Snake:
     def __init__(self, snake_width):
-        self.__x_change = 0
-        self.__y_change = 0
-        self.__body_x = [50]
-        self.__body_y = [50]
-        self.__width = snake_width
-        self.__course = "empty"
-        self.__length = len(self.__body_x)
-
-    def set_course(self, course):
-        step = 1
-        self.__course = course
-        if self.__course == "LEFT":
-            self.__x_change = -step
-            self.__y_change = 0
-        elif self.__course == "RIGHT":
-            self.__x_change = step
-            self.__y_change = 0
-        elif self.__course == "UP":
-            self.__y_change = -step
-            self.__x_change = 0
-        elif self.__course == "DOWN":
-            self.__y_change = step
-            self.__x_change = 0
-
-    def move(self):
-        i = len(self.__body_x) - 1
-        while(i > -1):
-            if (i == 0):
-                self.__body_x[i] += self.__x_change
-                self.__body_y[i] += self.__y_change
-                break
-            self.__body_x[i] = self.__body_x[i - 1]
-            self.__body_y[i] = self.__body_y[i - 1]
-            i -= 1
-
-    def eat(self):
-        for i in range(self.__width):
-            self.__body_x.append(self.__body_x[-1] - self.__x_change)
-            self.__body_y.append(self.__body_y[-1] - self.__y_change)
-
-    def get_head_space(self):
-        x0 = self.__body_x[0]
-        y0 = self.__body_y[0]
-        self.__space = [[x0, y0]]
-        for i in range(self.__width):
-            for j in range(self.__width):
-                self.__space.append([x0+i, y0+j])
-        return self.__space
-
-    def get_body_x(self):
-        return self.__body_x
-
-    def get_body_y(self):
-        return self.__body_y
-
-    def get_course(self):
-        return self.__course
-
-    def get_length(self):
-        return self.__length
-
-    def get_x_change(self):
-        return self.__x_change
-
-    def get_y_change(self):
-        return self.__y_change
-
-class CircularSnake:
-    def __init__(self, snake_width):
         self.__pos_change = [0, 0]
         self.__segment_pos = [[100, 100]]
         self.__width = snake_width
@@ -127,13 +58,13 @@ class CircularSnake:
         return self.__segment_pos
 
 class Food:
-    def __init__(self, snake_width, display_width, display_height):
+    def __init__(self, snake_width, display_width, display_height, inf_height):
         self.__size = snake_width
-        self.__radius = self.__size / 2
-        self.__d_w = display_width
-        self.__d_h = display_height
-        self.__x = random.randrange(self.__size, self.__d_w)
-        self.__y = random.randrange(self.__size, self.__d_h)
+        self.d_w = display_width
+        self.d_h = display_height
+        self.inf_height = inf_height
+        self.__x = random.randrange(self.__size, self.d_w)
+        self.__y = random.randrange(self.__size + inf_height, self.d_h)
         self.__pos = [self.__x, self.__y]
         self.size_factor = 3
 
@@ -153,8 +84,9 @@ class Food:
         pos_valid = False
         while 1 < 2:
             count = 0
-            x = random.randrange(self.__size, (self.__d_w-self.__size))
-            y = random.randrange(self.__size, (self.__d_h-self.__size))
+            x = random.randrange(self.__size, (self.d_w-self.__size))
+            y = random.randrange(self.inf_height + self.__size,
+                                 (self.d_h-self.__size))
             p = [x, y]
             for pos in excl:
                 if (dist(p, pos) >= near):
@@ -165,9 +97,6 @@ class Food:
             if count == 0:
                 break
         self.__pos = p
-
-    def get_radius(self):
-        return self.__radius
 
     def get_size(self):
         return self.__size
--- programs_py/games_py/snake.py
+++ programs_py/games_py/snake.py
@@ -1,130 +1,130 @@
 import pygame
 import random
 import time
+import math
+import items
 
 DISPLAY_WIDTH = 360
 DISPLAY_HEIGHT = 360
+INF_HEIGHT = 40
 X_CENTRE = DISPLAY_WIDTH / 2
 Y_CENTRE = DISPLAY_HEIGHT / 2
+FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-SNAKE_WIDTH = 10
+SNAKE_WIDTH = 20
 
 """Creating the main window."""
 pygame.init()
-pygame.mixer.init()
 screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("My snake")
 
 clock = pygame.time.Clock()
 
-"""Setting the coordinates of snake."""
-snake_x_change = 0
-snake_y_change = 0
-snake_body_x = [X_CENTRE]
-snake_body_y = [Y_CENTRE]
-food_x = random.randrange(0, DISPLAY_WIDTH, SNAKE_WIDTH)
-food_y = random.randrange(0, DISPLAY_HEIGHT, SNAKE_WIDTH)
+"""Setting snakes properties"""
+my_snake = items.Snake(SNAKE_WIDTH)
+body = my_snake.get_body()
+head_pos = my_snake.get_head_pos()
 
 """Setting other values"""
 score = 0
 course = ""
-fps = 5
+apple = items.Food(SNAKE_WIDTH, DISPLAY_WIDTH, DISPLAY_HEIGHT, INF_HEIGHT)
+
+def dist(a, b):
+    d = math.sqrt((b[0] - a[0])**2 +(b[1] - a[1])**2)
+    return d
 
 """ Creating the game cycle."""
 running = True
 while running:
+    """Keys binding"""
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             running = False
+        if event.type == pygame.KEYDOWN:
+            if (event.key==pygame.K_LEFT) and (course!="RIGHT"):
+                course = "LEFT"
+            elif (event.key==pygame.K_RIGHT) and (course!="LEFT"):
+                course = "RIGHT"
+            elif (event.key==pygame.K_UP) and (course!="DOWN"):
+                course = "UP"
+            elif (event.key==pygame.K_DOWN) and (course!="UP"):
+                course = "DOWN"
+            elif event.key == pygame.K_ESCAPE:
+                running = False
 
-    """Setting the course of movement."""
-    if event.type == pygame.KEYDOWN:
-        if (event.key==pygame.K_LEFT) and (course!="RIGHT"):
-            snake_x_change = -10
-            snake_y_change = 0
-            course = "LEFT"
-        elif (event.key==pygame.K_RIGHT) and (course!="LEFT"):
-            snake_x_change = 10
-            snake_y_change = 0
-            course = "RIGHT"
-        elif (event.key==pygame.K_UP) and (course!="DOWN"):
-            snake_y_change = -10
-            snake_x_change = 0
-            course = "UP"
-        elif (event.key==pygame.K_DOWN) and (course!="UP"):
-            snake_y_change = 10
-            snake_x_change = 0
-            course = "DOWN"
-        elif event.key == pygame.K_ESCAPE:
-            running = False
+    """Snake movement"""
+    my_snake.set_course(course)
+    my_snake.move()
 
-    """Wasted by collision a border."""
-    if ((snake_body_x[0] >= DISPLAY_WIDTH) or (snake_body_x[0] < 0) or
-           (snake_body_y[0] >= DISPLAY_HEIGHT) or
-           (snake_body_y[0] < 0)):
-         font = pygame.font.Font(None, 65)
-         text = "WASTED"
-         message = font.render(text, True, RED)
-         screen.blit(message, [X_CENTRE, Y_CENTRE])
-         pygame.display.update()
-         time.sleep(5)
-         running = False
+    """Snake eats food"""
+    h_pos = my_snake.get_head_pos()
+    a_pos = apple.get_pos()
+    a_radius = apple.get_size() / 2
+    d = dist(a_pos, h_pos)
+    if d < a_radius:
+        my_snake.eat(apple.get_size())
+        score += int(apple.get_size() / SNAKE_WIDTH)
+        apple.set_size(SNAKE_WIDTH)
+        apple.set_pos(body, SNAKE_WIDTH)
+
+
+
+    """Collision a border."""
+    if h_pos[0] == 0:
+        pos = [DISPLAY_WIDTH, h_pos[1]]
+        my_snake.set_pos(pos)
+    elif h_pos[0] == DISPLAY_WIDTH:
+        pos = [0, h_pos[1]]
+        my_snake.set_pos(pos)
+    if h_pos[1] == INF_HEIGHT:
+        pos = [h_pos[0], DISPLAY_HEIGHT]
+        my_snake.set_pos(pos)
+    elif h_pos[1] == DISPLAY_HEIGHT:
+        pos = [h_pos[0], INF_HEIGHT]
+        my_snake.set_pos(pos)
 
     """Wasted by collision the snakes body."""
-    for i in range(len(snake_body_x)):
-        if i != 0:
-            if ((snake_body_x[0] == snake_body_x[i]) and
-               (snake_body_y[0] == snake_body_y[i])):
-                 font = pygame.font.Font(None, 65)
-                 text = "WASTED"
-                 message = font.render(text, True, RED)
-                 screen.blit(message, [X_CENTRE, Y_CENTRE])
-                 pygame.display.update()
-                 time.sleep(5)
-                 running = False
-
-    """The snake is moving."""
-    iterator = len(snake_body_x) - 1
-    while(iterator > -1):
-        if (iterator == 0):
-            snake_body_x[iterator] += snake_x_change
-            snake_body_y[iterator] += snake_y_change
+    for i in range(SNAKE_WIDTH * 2, len(body)):
+        d = dist(body[i], h_pos)
+        if d < SNAKE_WIDTH:
+            font = pygame.font.Font(None, 20)
+            text = "Your score: " + str(score)
+            message = font.render(text, True, RED)
+            screen.blit(message, [X_CENTRE, Y_CENTRE])
+            pygame.display.update()
+            time.sleep(5)
+            running = False
             break
-        snake_body_x[iterator] = snake_body_x[iterator - 1]
-        snake_body_y[iterator] = snake_body_y[iterator - 1]
-        iterator -= 1
-
-    """The snake eat food."""
-    if (snake_body_x[0]==food_x) and (snake_body_y[0]==food_y):
-        food_x = random.randrange(0, DISPLAY_WIDTH, SNAKE_WIDTH)
-        food_y = random.randrange(0, DISPLAY_HEIGHT, SNAKE_WIDTH)
-        score = score + 1
-        snake_body_x.append(snake_body_x[-1] - snake_x_change)
-        snake_body_y.append(snake_body_y[-1] - snake_y_change)
-        fps = fps + 1
 
     """Frame out to the screen."""
+
     screen.fill(BLACK)
-
+    """Showing current informatoin"""
     font = pygame.font.Font(None, 30)
-    text = "Your score: " + str(score)
+    text = "Your score: " + str(score) + "      FPS: " + str(FPS)
     message = font.render(text, True, YELLOW)
-    screen.blit(message, [10, 10])
+    screen.blit(message, [6, 6])
 
-    for i in range(len(snake_body_x)):
-        pygame.draw.rect(screen, GREEN, [snake_body_x[i],
-                         snake_body_y[i], SNAKE_WIDTH, SNAKE_WIDTH])
+    s_rad = SNAKE_WIDTH / 2
+    pygame.draw.line(screen, YELLOW, (0, INF_HEIGHT - s_rad),
+                                      (DISPLAY_WIDTH, INF_HEIGHT - s_rad))
 
-    pygame.draw.rect(screen, RED, [food_x, food_y,
-                     SNAKE_WIDTH, SNAKE_WIDTH])
+    for segment in body:
+        pygame.draw.circle(screen, GREEN, (segment[0], segment[1]),
+                           (SNAKE_WIDTH/2), 0)
+
+    apple_pos = apple.get_pos()
+    pygame.draw.circle(screen, RED, (apple_pos[0], apple_pos[1]),
+                    apple.get_size()/2 , 0)
 
     pygame.display.update()
-    clock.tick(fps)
+    clock.tick(FPS)
 
 pygame.quit()
 quit()

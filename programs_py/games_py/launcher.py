dfb86b74a6afe2277d51b88428bd3e8025bff723
Ovod18 <ovod18@yandex.ru>
2022-01-27 20:20:21 +0400

    Added widgets and play button in launcher.py

--- programs_py/games_py/launcher.py
+++ programs_py/games_py/launcher.py
@@ -1,15 +1,69 @@
 import tkinter as tk
 import snake
 
-window = tk.Tk()
-label_1 = tk.Label(text = "Hello, World!")
-label_1.pack()
-
 def start_game():
     snake.main()
 
-button = tk.Button(window, text = "Start", command = start_game,
-                               width = 25, height = 5,
-                               bg = "blue", fg = "yellow")
-button.pack()
-window.mainloop()
+"""Seting start window"""
+start_window = tk.Tk()
+
+"""Welcome"""
+frame1 = tk.Frame(master = start_window,
+                  bg = "black")
+
+welcome_lbl = tk.Label(master = frame1,
+                       width = 27,
+                       text = 'Welcome to the "Snake" game!',
+                       bg = "black",
+                       fg = "yellow")
+welcome_lbl.pack()
+
+"""Setting the input data"""
+setting_frame= tk.Frame(master = start_window,
+                        bg = "black")
+
+display_width_lbl = tk.Label(master = setting_frame,
+                             width = 27,
+                             text = "Display width",
+                             bg = "black",
+                             fg = "yellow")
+display_width_lbl.pack()
+
+display_width_entry= tk.Entry(master = setting_frame)
+display_width_entry.insert(0, "200")
+display_width_entry.pack()
+
+display_height_lbl = tk.Label(master = setting_frame,
+                              text = "Display height",
+                              bg = "black",
+                              fg = "yellow")
+display_height_lbl.pack()
+
+display_height_entry= tk.Entry(master = setting_frame)
+display_height_entry.insert(0, "200")
+display_height_entry.pack()
+
+snake_width_lbl = tk.Label(master = setting_frame,
+                           text = "Snake width",
+                           bg = "black",
+                           fg = "yellow")
+snake_width_lbl.pack()
+
+snake_width_entry = tk.Entry(master = setting_frame)
+snake_width_entry.insert(0, "20")
+snake_width_entry.pack()
+
+
+"""Start the game"""
+play_btn = tk.Button(master = setting_frame,
+                     text = "Play",
+                     command = start_game,
+                     width = 24,
+                     height = 3,
+                     bg = "blue",
+                     fg = "yellow")
+play_btn.pack()
+
+frame1.pack()
+setting_frame.pack()
+start_window.mainloop()

0090a406275613e3fbc2d4dc34199b9a88350393
Ovod18 <ovod18@yandex.ru>
2022-02-11 14:32:19 +0400

    Added docstrings in launcher.py and created documentation for launcher.py

--- snake/launcher.py
+++ snake/launcher.py
@@ -17,8 +17,7 @@ dw, dh, sw = 200, 200, 20
 """
 
 def is_valid(dw, dh, sw):
-    """
-    Validation input data.
+    """Validation input data.
 
     :param int dw: The display width
     :param int dh: The display height
@@ -26,6 +25,7 @@ def is_valid(dw, dh, sw):
     :returns: True or False
     :rtype: boolean
     """
+
     if (dw>=DISPLAY_MIN) and (dh>=DISPLAY_MIN) and (sw>=SNAKE_MIN):
         if ((sw <= (dw//10)) or (sw <= (dh//10))):
             return True
@@ -34,9 +34,8 @@ def is_valid(dw, dh, sw):
     else:
         return False
 
-
 def btn_play_click():
-    """The handler of btn_play click"""
+    """The handler of btn_play click."""
 
     try:
         dw = int(display_width_entry.get())
@@ -54,43 +53,36 @@ def btn_play_click():
         mb.showerror("Error", msg)
 
 def start(event):
-    """
-    Start 'btn_play_click' with some event.
+    """Start 'btn_play_click' with some event.
 
-    Parameters
-    ----------
-    event : object
-        event is some event.
-        For exaple: if 'Enter' key is pressed,
-            then event will be '<Return>'.
+    :param object event: some event
     """
+
     btn_play_click()
 
-
-
 def start_game(dw, dh, sw, ss):
-    """
-    Start the game.
+    """Start the game with following parameters.
 
-    Parameters
-    ----------
-    dw, dh, sw, ss : int
-        The width, the height of display and the width, the speed of snake.
+    :param int dw: display width
+    :param int dh: display height
+    :param int sw: snake width
+    :param int ss: snake width
     """
 
     snake.main(dw, dh, sw, ss)
 
-"""Seting start window."""
+# Seting start window.
 start_window = tk.Tk()
 start_window.title("launcher")
 
-"""Start 'btn_play_click' with some event'"""
+# Start 'btn_play_click' with some event'
 start_window.bind("<Return>", start)
 
 frame1 = tk.Frame(master = start_window,
                   bg = "black")
+frame1.pack()
 
-"""The label "Welcome to the "Snake" game!" """
+# The label "Welcome to the "Snake" game!".
 welcome_lbl = tk.Label(master = frame1,
                        width = 27,
                        text = 'Welcome to the "Snake" game!',
@@ -98,12 +90,12 @@ welcome_lbl = tk.Label(master = frame1,
                        fg = "yellow")
 welcome_lbl.pack()
 
-
-"""Setting the input data."""
+# Setting the input data.
 setting_frame= tk.Frame(master = start_window,
                         bg = "black")
+setting_frame.pack()
 
-"""The label "Display width" """
+# The label "Display width".
 display_width_lbl = tk.Label(master = setting_frame,
                              width = 27,
                              text = "Display width (>="+str(DISPLAY_MIN) +
@@ -112,12 +104,12 @@ display_width_lbl = tk.Label(master = setting_frame,
                              fg = "yellow")
 display_width_lbl.pack()
 
-"""The text box which sets the display width"""
+# The text box which sets the display width.
 display_width_entry= tk.Entry(master = setting_frame)
 display_width_entry.insert(0, "200")
 display_width_entry.pack()
 
-"""The label "Display height" """
+# The label "Display height".
 display_height_lbl = tk.Label(master = setting_frame,
                               text = "Display height (>="+str(DISPLAY_MIN)+
                                       ")",
@@ -125,37 +117,37 @@ display_height_lbl = tk.Label(master = setting_frame,
                               fg = "yellow")
 display_height_lbl.pack()
 
-"""The text box which sets the display height"""
+# The text box which sets the display height.
 display_height_entry= tk.Entry(master = setting_frame)
 display_height_entry.insert(0, "200")
 display_height_entry.pack()
 
-"""The label "Snake width" """
+# The label "Snake width"
 snake_width_lbl = tk.Label(master = setting_frame,
                            text = "Snake width (>=" + str(SNAKE_MIN) + ")",
                            bg = "black",
                            fg = "yellow")
 snake_width_lbl.pack()
 
-"""The text box which sets snake width"""
+# The text box which sets snake width
 snake_width_entry = tk.Entry(master = setting_frame)
 snake_width_entry.insert(0, "20")
 snake_width_entry.pack()
 
-"""The label "Snake speed """
+# The label "Snake speed 
 snake_speed_lbl = tk.Label(master = setting_frame,
                            text = "Snake speed",
                            bg = "black",
                            fg = "yellow")
 snake_speed_lbl.pack()
 
-"""The text box which sets snake speed"""
+# The text box which sets snake speed
 snake_speed_entry = tk.Entry(master = setting_frame)
 snake_speed_entry.insert(0, "1")
 snake_speed_entry.pack()
 
 
-"""The button of start the game."""
+# The button of start the game.
 play_btn = tk.Button(master = setting_frame,
                      text = "Play",
                      command = btn_play_click,
@@ -163,12 +155,11 @@ play_btn = tk.Button(master = setting_frame,
                      height = 3,
                      bg = "blue",
                      fg = "yellow")
+play_btn.pack()
 
 def main():
     """The main function in launcher.py"""
-    play_btn.pack()
-    frame1.pack()
-    setting_frame.pack()
+
     start_window.mainloop()
 
 if __name__ == "__main__":
